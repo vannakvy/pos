2b770bc663a5a82cb6eed57b743e7d5fb01a48a2
vannakvy <vannakvy2020@gmail.com>
2021-01-25 09:13:48 +0700

    change

--- backend/routes/eBookRoutes/eBookStaticFileRoutes.js
+++ backend/routes/eBookRoutes/eBookStaticFileRoutes.js
@@ -6,12 +6,14 @@ import multiparty from 'connect-multiparty';
 import path from 'path';
 import * as fs from 'fs';
 const dirname = path.dirname(new URL(import.meta.url).pathname);
+console.log(dirname)
 const __dirname= dirname.substring(0,dirname.length-27);
+console.log(__dirname)
 // console.log(a)
 // const __dirname =dirname.substring(0,7)
 console.log(__dirname)
 
-const MultiPartyMiddleware = multiparty({uploadDir:'/images'});
+const MultiPartyMiddleware = multiparty();
 
 const router = express.Router()
 router.use(bodyparser.json());
@@ -22,7 +24,7 @@ router.post('/',MultiPartyMiddleware,(req,res)=>{
     var Tempfile = req.files.upload;
     var TempPathFile = Tempfile.path;
     const taretPathUrl = path.join(__dirname,"/uploads/eBookUploads/"+Tempfile.name);
-
+    console.log(taretPathUrl);
     if(path.extname(Tempfile.originalFilename).toLowerCase() ===".png"||"jpg"){
         let newPath = taretPathUrl.substring(1);
         fs.rename(TempPathFile,newPath,err=>{
