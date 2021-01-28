21efa74801fde3fca688f80c53cf8a44a69e1bb0
vannakvy <vannakvy2020@gmail.com>
2021-01-28 11:38:29 +0700

    DDD

--- backend/routes/eShopRoutes/inventoryRoutes.js
+++ backend/routes/eShopRoutes/inventoryRoutes.js
@@ -1,15 +1,8 @@
 import express from 'express'
 const router = express.Router()
-import {getPuchases,addPuchases} from '../../controllers/eShopControllers/inventoryControllers.js'
+import {getPuchases,addPuchases,updatePuchase,deletePuchase, getOnePuchase} from '../../controllers/eShopControllers/inventoryControllers.js'
 import { protect, admin } from '../../middleware/authMiddleware.js'
 
 router.route('/puchases').get(getPuchases).post(addPuchases)
-// router.route('/:id/reviews').post(protect, createProductReview)
-// router.get('/top', getTopProducts)
-// router
-//   .route('/:id')
-//   .get(getProductById)
-//   .delete(protect, admin, deleteProduct)
-//   .put(protect, admin, updateProduct)
-
+router.route('/puchases/:id').get(getOnePuchase).put(updatePuchase).delete(deletePuchase)
 export default router
