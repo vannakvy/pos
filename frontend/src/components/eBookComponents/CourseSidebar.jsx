e1161274bb9c5339abcecd1851239ae5dde48b36
jrnn21 <chamroeuncl22@gmail.com>
2021-06-14 20:33:14 +0700

    new ebook

--- frontend/src/components/eBookComponents/CourseSidebar.jsx
+++ frontend/src/components/eBookComponents/CourseSidebar.jsx
@@ -1,36 +1,52 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import "./Sidebar.css";
-import Loader from "../Loader";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import './Sidebar.css';
+import Loader from '../Loader';
 
 function CourseSidebar({ loading, courses, action }) {
-  return (
-    <div className="sidebar ml-1 mt-1">
-      {loading ? (
+ return (
+  <div
+   className="sidebar ml-1 mt-1 sticky-top"
+   style={{ top: 76, height: '92vh', overflowY: 'auto' }}
+  >
+   {/* {loading ? (
         <Loader wd={80} hg={80} />
-      ) : (
-        <>
-          <ul className="list-group list-group-flush">
-            {courses &&
-              courses.map((course) => (
-                <>
-                  <Link
-                    onClick={() => action(course._id)}
-                    key={course._id}
-                    className="link"
-                  >
-                    <li className="list-group-item courseList">
-                      {course.title}
-                    </li>
-                  </Link>
-                </>
-              ))}
-          </ul>
-        </>
-      )}
-    </div>
-  );
+      ) : ( */}
+   <>
+    <ul className="list-group list-group-flush pb-5 bg-light">
+     {courses &&
+      courses.map((course) => (
+       <>
+        <Link
+         onClick={() => action(course._id)}
+         key={course._id}
+         className="link"
+        >
+         <li className="list-group-item py-2 pl-3 adminHover">
+          {course.title}
+         </li>
+        </Link>
+       </>
+      ))}
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+     <li className="list-group-item py-2 pl-3 adminHover">gg</li>
+    </ul>
+   </>
+   {/* )} */}
+  </div>
+ );
 }
 
 export default CourseSidebar;
