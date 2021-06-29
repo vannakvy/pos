2e3e3ae128285cb126bfcec9dd0471378e629620
vannakvy <vannakvy2020@gmail.com>
2021-06-29 20:55:59 +0700

    testing

---
+++ db_note.js
@@ -0,0 +1,59 @@
+{
+    "taxPrice": 3.9,
+    "shippingPrice": 100,
+    "totalPrice": 129.9,
+    "isPaid": true,
+    "isDelivered": true,
+    "orderItems": [{
+        "_id": {
+            "$oid": "6007ddeb934cf9060ce49e8c"
+        },
+        "product": {
+            "$oid": "60077a33739c7e2880036ce6"
+        },
+        "name": "Sample name",
+        "image": "/uploads\\eShopUploads\\image-1611102913566.jpg",
+        "price": 4,
+        "qty": 5
+    }, {
+        "_id": {
+            "$oid": "6007ddeb934cf9060ce49e8d"
+        },
+        "product": {
+            "$oid": "6007799c739c7e2880036ce5"
+        },
+        "name": "Sample name",
+        "image": "/uploads\\eShopUploads\\image-1611102742302.jpg",
+        "price": 2,
+        "qty": 3
+    }],
+    "user": {
+        "$oid": "5ff81780c7dcbe4c3402e027"
+    },
+    "shippingAddress": {
+        "address": "Preah dark",
+        "city": "Siem reap",
+        "postalCode": "123",
+        "country": "Cambodia"
+    },
+    "paymentMethod": "PayPal",
+    "createdAt": {
+        "$date": "2021-01-20T07:38:19.513Z"
+    },
+    "updatedAt": {
+        "$date": "2021-01-20T07:43:45.300Z"
+    },
+    "__v": 0,
+    "paidAt": {
+        "$date": "2021-01-20T07:41:57.871Z"
+    },
+    "paymentResult": {
+        "id": "8WY414575N827881Y",
+        "status": "COMPLETED",
+        "update_time": "2021-01-20T07:41:55Z",
+        "email_address": "sb-0ilg53443762@personal.example.com"
+    },
+    "deliveredAt": {
+        "$date": "2021-01-20T07:43:45.294Z"
+    }
+}
