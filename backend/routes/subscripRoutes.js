d20974709ed9b4f0eb2fb2126a43e2594d9f3698
jrnn21 <chamroeuncl22@gmail.com>
2021-09-06 20:41:24 +0700

    dd

--- backend/routes/subscripRoutes.js
+++ backend/routes/subscripRoutes.js
@@ -4,6 +4,6 @@ import { admin, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(protect, createSubscrip);
+router.route('/').post(createSubscrip);
 
 export default router;
